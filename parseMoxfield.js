f57385cd9d0b20b67404907454fcefa3f77cfbd0
liperuf <liperuf@gmail.com>
2022-05-12 17:39:06 -0300

    Increasing failsafes

--- parseMoxfield.js
+++ parseMoxfield.js
@@ -9,6 +9,9 @@ export default async function parseMoxfield(decklistReference) {
   const response = await fetch(`https://api.moxfield.com/v2/decks/all/${decklistId}`).then(response => response.json());
 
   if(!response) return [];
+  if(!response.mainboard) return [];
+  if(!response.commanders) return [];
+  if(!response.companions) return [];
 
   return [
     ...Object.entries(response.mainboard).map(key => key[0]),
